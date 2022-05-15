a17b8ba6113022381c8be2254dbb206965b43113
themarkr <markrodriguezaa@gmail.com>
2022-05-15 02:49:01 -0400

    signup route added

--- dreambridge/src/App.js
+++ dreambridge/src/App.js
@@ -4,12 +4,13 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Lawyers from './pages/lawyers';
 import Login from './pages/login';
 import LawyerProfile from './components/LawyerProfile';
-
+import SignUp from './components/SignUp';
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path='/' element={<Login />}/>
+        <Route exact path='/signup' element={<SignUp/>}/>
         <Route exact path='/Connect-with-a-lawyer' element={<Lawyers />}/>
         <Route path='Connect-with-a-lawyer/lawyers/:id' element={<LawyerProfile />}/>
         {/* <Route exact path='/userprofile' element={UserProfile />}/> */}
