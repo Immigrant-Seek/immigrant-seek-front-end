a9da9d4a25fb39042229ed5d29271d20d3d751ff
themarkr <markrodriguezaa@gmail.com>
2022-05-15 19:45:53 -0400

    create login component and functionality

--- dreambridge/src/pages/login.js
+++ dreambridge/src/pages/login.js
@@ -1,20 +1,62 @@
 import { Button, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import Context from '../context/Context';
+import React from 'react';
 
 // This is where you render the Login Form component you created
 
 function Login(){
+    const [userInfo, setUserInfo] = React.useState({})
+    const context = React.useContext(Context);
+
+    // when a user submits the form, we want to grab all the data from the form and update the state of the userInfo
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const userEmail = event.target.email.value;
+        const userPassword = event.target.passwrd.value;
+        console.log(userEmail, userPassword);
+
+        setUserInfo({
+            email : userEmail,
+            password : userPassword
+        })
+    }
+
+    const loginAttempt = async(userData) => {
+        const response = await fetch("http://localhost:3030/login", {
+            method : "POST",
+            headers: {
+                "Content-Type" : "application/json",
+            },
+            body: JSON.stringify(userData)
+        })
+        const data = await response.json()
+        return data;
+    }
+
+    React.useEffect(() => {
+        loginAttempt(userInfo).then(data => {
+            context.updateToken(data.token)
+            context.updateVerifiedUser(data)
+        })
+    },[userInfo])
+
+    React.useEffect(() => {
+        console.log(context.token);
+        console.log(context.verifiedUser);
+    },[context.token])
+
     return (
         <div>
         <h2>Sign In</h2>
-        <Form>
+        <Form onSubmit = {handleSubmit}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Control name ="email" type="email" placeholder="Enter email" />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control name ="passwrd"  type="password" placeholder="Password" />
             </Form.Group>
             <Button variant="primary" type="submit" >
                 Submit
@@ -22,7 +64,7 @@ function Login(){
         </Form>
         <span>Don't have an account yet? Click <Link to="/signup">here</Link> to sign up </span>
         {/* This is a temp link */}
-        {/* <Link to="/Connect-with-a-lawyer">Click to go to main page</Link> */}
+        <Link to="/Connect-with-a-lawyer">Click to go to main page</Link>
     </div>
     )
 }
