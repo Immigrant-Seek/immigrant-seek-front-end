6ef668914c91ba68d8e4a59ce529cc1209f52a54
lreboseno22 <liamrebo22@gmail.com>
2022-05-16 21:28:49 -0400

    rendered reviews / post reviews / filter options

--- dreambridge/src/context/ContextProvider.js
+++ dreambridge/src/context/ContextProvider.js
@@ -2,20 +2,12 @@ import { useState, useEffect } from "react";
 import Context from "./Context";
 
 function ContextProvider({children}){
-    // const [ lawyers, setLawyers ] = useState([])
-
-    // useEffect(() => {
-    //     fetch("http://localhost:3030/lawyers")
-    //     .then(res => res.json())
-    //     .then((data) => {
-    //         console.log(data);
-    //         setLawyers(data.data)
-    //       })
-    // }, []);
-
-    const [ listOfLawyers, updateLawyerList ] = useState([])
+    const [ listOfLawyers, updateLawyerList ] = useState([]);
     const [ token, updateToken ] = useState("");
     const [verifiedUser, updateVerifiedUser] = useState({});
+    const [ listOfStates, updateStatesList ] = useState([]);
+    const [ listOfFirms, updateFirmsList ] = useState([]);
+
     const getAllLawyers = async() => {
         const res =  await fetch("http://localhost:3030/lawyers")
         const data = await res.json();
@@ -28,11 +20,40 @@ function ContextProvider({children}){
         })
     }, [])
 
+    const getAllStates = async() => {
+        const res = await fetch("http://localhost:3030/states")
+        const data = await res.json();
+        return data.data
+    }
+
+    useEffect(() => {
+        getAllStates().then(states => {
+            updateStatesList(states)
+        })
+    }, [])
+
+    const getAllFirms = async() => {
+        const res = await fetch("http://localhost:3030/firms")
+        const data = await res.json();
+        return data.data
+    }
+
+    useEffect(() => {
+        getAllFirms().then(firms => {
+            updateFirmsList(firms)
+        })
+    })
+
     const state = {
-        // lawyers, setLawyers
         getAllLawyers, 
         listOfLawyers, 
         updateLawyerList,
+        getAllStates,
+        listOfStates,
+        updateStatesList,
+        getAllFirms,
+        listOfFirms,
+        updateFirmsList,
         token,
         updateToken,
         verifiedUser,
