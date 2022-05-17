230dace36a5f0119d1ba7563c1d30ff9478be258
lreboseno22 <liamrebo22@gmail.com>
2022-05-17 15:07:15 -0400

    added client name to review

--- dreambridge/src/components/ReviewCard.js
+++ dreambridge/src/components/ReviewCard.js
@@ -1,24 +1,24 @@
-// import { useEffect, useState } from "react";    
+import { useEffect, useState } from "react";    
 
-// function ReviewCard(props){
-//     const { reviews, lawyer } = props;
+function ReviewCard(props){
+    const { review } = props;
 
-//     console.log(lawyer);
-//     cosole.log(reviews);
-//     const [ currentReview, setCurrentReview ] = useState([])
+    const [ loadedReview, setLoadedReview ] = useState([])
 
-//     useEffect(() => {
-//         fetch(`http://localhost:3030/lawyers/${lawyer.user_id}/reviews`)
-//         .then(res => res.json())
-//         .then((data) => {
-//             setCurrentReview(data)
-//         })
-//     }, [])
+    useEffect(() => {
+        fetch(`http://localhost:3030/review/${review.review_id}/user`)
+        .then(res => res.json())
+        .then((data) => {
+            setLoadedReview(data)
+        })
+    }, [])
 
+    return (
+        <>
+            <p key={review.review_id}>{review.review_body}</p> 
+            <p className="card-text"><small className="text-muted">Reviewed by user: {review.first_name}</small></p>
+        </>
+    )
+}
 
-//     return (
-//         <div>
-//             <p review_body></p>
-//         </div>
-//     )
-// }
+export default ReviewCard
