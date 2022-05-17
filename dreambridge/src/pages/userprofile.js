3551d119503a9bb66a83a8151a3b3b5dc5c4abd3
Jared Moore <jvredmoore@Jareds-MacBook-Air.local>
2022-05-17 14:38:24 -0400

    pulled down from main

---
+++ dreambridge/src/pages/userprofile.js
@@ -0,0 +1,17 @@
+import ClientProfile from '../components/ClientProfile';
+import Nav from '../components/Nav';
+
+
+
+function Clients(){
+    
+
+    return (
+      <div className='App'>
+        <Nav/>
+        <ClientProfile />
+        </div>
+    );
+  }
+  
+  export default Clients;
